9a272c64d31bf258aee69922ef98ed46dc70004e
RulerSteps <diagnemalick163@gmail.com>
2026-05-18 16:16:13 +0000

    Rapports lab3 et lab 4

---
+++ LAB3/app/src/main/java/com/example/bibliothequeapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.bibliothequeapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Affiche le layout XML
+        setContentView(R.layout.activity_main);
+
+        // Récupération du bouton
+        Button btnVoirLivres = findViewById(R.id.btnVoirLivres);
+
+        // Action lors du clic
+        btnVoirLivres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Affiche un message
+                Toast.makeText(MainActivity.this,
+                        "Accès à la liste des livres",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
